c670a02bc7f8664b3697c14cfa60e377c438edba
Alex Ozdemir <aozdemir@hmc.edu>
2019-12-13 10:11:54 -0800

    New merge

--- gg/merge.cc
+++ gg/merge.cc
@@ -1,21 +1,45 @@
-#include <iostream>
+#include <cstring>
 #include <fstream>
+#include <iostream>
 #include <unistd.h>
 
 using namespace std;
 
-int main (int argc, char *argv[]) {
-    ofstream o{"out"};
-    for (size_t i = 1; i < argc; ++i) {
-        string content;
-        ifstream in{argv[i]};
-        in >> content;
-        if (content == "SAT") {
-            o << "SAT\n";
-            o << in.rdbuf();
-            return EXIT_SUCCESS;
+bool is_thunk(const string& path)
+{
+    constexpr char magic_number[] = "##GGTHUNK##";
+    constexpr size_t magic_number_len = sizeof(magic_number) / sizeof(char) - 1;
+    char first_bytes[magic_number_len] = { 0 };
+
+    ifstream fin{ path };
+    fin.read(first_bytes, magic_number_len);
+
+    return (strncmp(magic_number, first_bytes, magic_number_len) == 0);
+}
+
+int main(int argc, char* argv[])
+{
+    ofstream o{ "out" };
+    bool some_thunk_remains = false;
+    for (int i = 1; i < argc; i++) {
+        if (is_thunk(argv[i])) {
+            some_thunk_remains = true;
+        } else {
+            ifstream fin{ argv[i] };
+            string content;
+            fin >> content;
+            if (content == "SAT") {
+                o << "SAT\n";
+                o << fin.rdbuf();
+                return EXIT_SUCCESS;
+            }
         }
     }
-    o << "UNSAT\n";
+    if (some_thunk_remains) {
+        ifstream tin{ getenv("__GG_THUNK_PATH__") };
+        o << tin.rdbuf();
+    } else {
+        o << "UNSAT\n";
+    }
     return EXIT_SUCCESS;
 }
