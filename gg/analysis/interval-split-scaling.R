6ecef0cc9fb205f1fe6e426794b825aaf20ae952
Alex Ozdemir <aozdemir@hmc.edu>
2020-01-22 13:49:57 -0800

    Interval splitting plot

--- gg/analysis/interval-split-scaling.R
+++ gg/analysis/interval-split-scaling.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 data <- read_csv("./data.csv")
-benches <- c("2_6","4_2","3_5","4_5","5_5","mnist")
+benches <- c("2_6","4_2","3_5","4_5","5_5")
 regex <- str_c(benches, collapse="|")
 right_benches <- data %>% filter(grepl(regex, net))
 no_outliers <- right_benches %>%
@@ -15,20 +15,24 @@ no_outliers <- right_benches %>%
   filter(trial != 1 | !grepl("5_5", net) | jobs !=1000) %>%
   filter(trial != 1 | !grepl("5_5", net) | jobs !=512) %>%
   filter(hash != "76fe1c29" | !grepl("4_5", net) | jobs !=512)
+no_outliers[no_outliers == 1024] <- 1000
 series <- no_outliers %>%
-  group_by(jobs,infra,net) %>%
-  summarise(mean_rt = median(runtime), sd_rt = sd(runtime))
-series[is.na(series)] <- 0
-ggplot(series, aes(x = jobs, y = mean_rt, color = infra, linetype = net)) +
+  group_by(jobs,infra,divide_strategy,net) %>%
+  summarise(rt_mean_trial = mean(runtime), rt_sd_trial = sd(runtime)) %>%
+  group_by(jobs,infra,divide_strategy) %>%
+  summarise(rt_mean_net = mean(rt_mean_trial), rt_sd_net = sd(rt_sd_trial))
+
+#series[is.na(series)] <- 0
+ggplot(series, aes(x = jobs, y = rt_mean_net, linetype = infra)) +
   geom_line() +
-  geom_errorbar(aes(ymax = mean_rt + sd_rt, ymin = mean_rt - sd_rt)) +
+  geom_point() +
   scale_x_continuous(trans='log2') +
-  scale_y_continuous(trans='log2') +
+  scale_y_continuous(trans='log2', limits =c(32, 4096)) +
   labs(
     title = "Runtime improvement as parallelism grows",
     y = "Runtime (s)",
     x = "Number of Workers",
-    linetype = "Network (Proprty 1)",
-    color = "Infrastructure"
+    linetype = "Infrastructure",
+    color = "Split Strategy"
   )
-    
+ggsave("interval-split-scaling.png", width = 5, height = 4, units="in")    
