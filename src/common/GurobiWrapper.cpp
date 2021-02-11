9c6a5a83065abdb1d9cf97d88053e131a1848371
anwu1219 <haozewu@stanford.edu>
2021-02-11 09:44:41 -0800

    print

--- src/common/GurobiWrapper.cpp
+++ src/common/GurobiWrapper.cpp
@@ -259,10 +259,12 @@ void GurobiWrapper::solve()
 {
     try
     {
-        _model->update();
-        printf( "Number of constraints: %u\n", _model->get( GRB_IntAttr_NumConstrs ) );
-        printf( "Number of variables: %u\n", _model->get( GRB_IntAttr_NumVars ) );
-        printf( "Number of non-zeros: %u\n", _model->get( GRB_IntAttr_NumNZs ) );
+        DEBUG({
+            _model->update();
+            printf( "Number of constraints: %u\n", _model->get( GRB_IntAttr_NumConstrs ) );
+            printf( "Number of variables: %u\n", _model->get( GRB_IntAttr_NumVars ) );
+            printf( "Number of non-zeros: %u\n", _model->get( GRB_IntAttr_NumNZs ) );
+            });
         _model->optimize();
     }
     catch ( GRBException e )
