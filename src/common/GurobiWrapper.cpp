c98903953e358142c9a0c8f80a24979d24da89eb
anwu1219 <haozewu@stanford.edu>
2021-03-10 10:52:43 -0800

    increase precision

--- src/common/GurobiWrapper.cpp
+++ src/common/GurobiWrapper.cpp
@@ -80,6 +80,12 @@ void GurobiWrapper::resetModel()
     _model->getEnv().set( GRB_IntParam_Threads,
                           GlobalConfiguration::GUROBI_NUMBER_OF_THREADS );
 
+    // Thread precision
+    _model->getEnv().set( GRB_DoubleParam_FeasibilityTol,
+                          GlobalConfiguration::DEFAULT_EPSILON_FOR_COMPARISONS );
+    _model->getEnv().set( GRB_DoubleParam_IntFeasTol,
+                          GlobalConfiguration::RELU_CONSTRAINT_COMPARISON_TOLERANCE );
+
     // Timeout
     setTimeLimit( _timeoutInSeconds );
 }
